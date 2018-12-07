aa9538ba9e044b01824fa38c51fe715d1f93ba57
Tin Cya <32213532+nghichcode@users.noreply.github.com>
2018-12-07 09:16:31 +0000

    v5
    v5

--- index.js
+++ index.js
@@ -5,27 +5,35 @@ var querystring = require('querystring');
 var login = require('./login');
 var logout = require('./logout');
 var picking = require('./picking');
-var err = false;
 
 http.createServer(function (req, res) {
 
 	// POST data
-    if(req.method === "POST") {
+	const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+
+    if(req.headers['content-type'] && req.headers['content-type'] === FORM_URLENCODED && req.method === "POST") {
     	var body = '';
         req.on('data', function(chunk) {body += chunk.toString();});
         req.on('end', function() {response(querystring.parse(body));});
-    } else {err = true;}
+    } else {
+		res.writeHead(200,{'Content-Type':'text/json'});
+		res.write('{"status": "wrongHeads","statusText":"Wrong Headers"}');
+		res.end();
+    }
 
     function response(params) {
     	var resContent = {};
     	var urlParams = url.parse(req.url, true);
 
 		res.writeHead(200,{'Content-Type':'text/json'});
-		var correctToken =  params.username && params.password && params.client_id && params.client_secret && params.grant_type;
+		var correctToken =  params.username && params.password || params.client_id || params.client_secret || params.grant_type;
 		if (!req.headers['authorization']) {return;};
 		var correctRevoke =  req.headers['authorization'];
 		var correctPicking = req.headers['authorization'] && params.whCode && params.customerCode && params.pickingNo;
 
+		console.log(urlParams.pathname);
+		console.log(correctToken);
+		console.log(params);
 		if (urlParams.pathname == "/oauth/token" && correctToken) {
 			resContent = login.parse(params);
 		} else if (urlParams.pathname == "/oauth/revoke" && correctRevoke) {
@@ -38,11 +46,6 @@ http.createServer(function (req, res) {
 		res.write(JSON.stringify(resContent));
 		res.end();
     }
-
-    if (err) {
-		res.writeHead(200,{'Content-Type':'text/json'});
-		res.end("Err");
-    };
 
 }).listen(process.env.PORT || 5000);
 // console.log(body);
