23b609e2bad75de937cfb13230eafe8c64b7ecc5
Tin Cya <32213532+nghichcode@users.noreply.github.com>
2018-12-07 08:32:16 +0000

    v2
    v2

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-var express = require('express')
-var app = express()
+var http = require('http');
+var url= require('url');
+var querystring = require('querystring');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var login = require('./login');
+var logout = require('./logout');
+var picking = require('./picking');
+var err = false;
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+http.createServer(function (req, res) {
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+	// POST data
+	const FORM_URLENCODED = 'application/json; charset=utf-8';
+    if(req.headers['content-type'] === FORM_URLENCODED && req.method === "POST") {
+    	var body = '';
+        req.on('data', function(chunk) {body += chunk.toString();});
+        req.on('end', function() {response(querystring.parse(body));});
+    } else {err = true;}
+
+    function response(params) {
+    	var resContent = {};
+    	var urlParams = url.parse(req.url, true);
+
+		res.writeHead(200,{'Content-Type':'text/json'});
+		var correctToken =  params.username && params.password && params.client_id && params.client_secret && params.grant_type;
+		var correctRevoke =  req.headers['authorization'];
+		var correctPicking = req.headers['authorization'] && params.whCode && params.customerCode && params.pickingNo;
+
+		if (urlParams.pathname == "/oauth/token") {
+			resContent = login.parse(params);
+		} else if (urlParams.pathname == "/oauth/revoke" && correctRevoke) {
+			resContent = logout.parse(req.headers['authorization']);
+		} else if (urlParams.pathname == "/api/picking/sacnSerial.json" && correctPicking) {
+			resContent = picking.parse(req.headers['authorization'], params);
+		} else {
+			resContent = {status: "wrongParams",statusText:"Wrong Parameters"};
+		};
+		res.write(JSON.stringify(resContent));
+		res.end();
+    }
+
+    if (err) {
+		res.writeHead(200,{'Content-Type':'text/json'});
+		res.end("Err");
+    };
+
+}).listen(process.env.PORT || 5000);
+// console.log(body);
