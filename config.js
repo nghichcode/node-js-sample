aeb8b26e391462f4a7f606c3a6605307a19a981c
Tin Cya <32213532+nghichcode@users.noreply.github.com>
2018-12-12 07:11:17 +0000

    v112
    hfg

--- config.js
+++ config.js
@@ -4,14 +4,17 @@ exports.getTime = function() {
 	var now="Errrr";
 
 	const client = new Client({
-	  connectionString: process.env.DATABASE_URL || "postgres://khxbijdhccsjkc:80fbc29fa8a5273e15db91d3b704ca76e7d7c8a024bb128b0d8342afaeb18a7f@ec2-54-204-40-248.compute-1.amazonaws.com:5432/d77n0qu161p9b3"
+	  connectionString: process.env.DATABASE_URL
 	});
 	client.connect();
 
 	client.query('SELECT NOW()', function(err, res) {
 		console.log("query");
-		if (err) {now ={status: "Error",statusText:"getTime Err"} ;return;}
-		now = {status: "Error",statusText:"getTime OK"};
+		if (err) {
+			now ={status: "Error",statusText:"getTime Err"} ;
+		} else {
+			now = {status: "Error",statusText:"getTime OK"};		
+		}
 	});
 
 	return now;
