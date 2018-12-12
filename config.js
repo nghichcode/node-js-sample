6cd2d0aa3414b56cdef48db33939f334bf3ea973
Tin Cya <32213532+nghichcode@users.noreply.github.com>
2018-12-12 07:14:36 +0000

    V115
    DSA

--- config.js
+++ config.js
@@ -3,17 +3,19 @@ const {Client} = require('pg');
 exports.getTime = function() {
 	var now="Errrr";
 
+	now = {status: "Error",statusText:"getTime 2"};
 	const client = new Client({
 	  connectionString: process.env.DATABASE_URL
 	});
 	client.connect();
+	now = {status: "Error",statusText:"getTime 3"};
 
 	client.query('SELECT NOW()', function(err, res) {
 		console.log("query");
 		if (err) {
 			now ={status: "Error",statusText:"getTime Err"} ;
 		} else {
-			now = {status: "Error",statusText:"getTime OK"};		
+			now = {status: "Error",statusText:"getTime OK"};
 		}
 	});
 
