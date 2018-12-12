dad2885f3bedc5f1cb5300fa04aed00ac091834e
Tin Cya <32213532+nghichcode@users.noreply.github.com>
2018-12-12 04:51:22 +0000

    V115
    VV

--- config.js
+++ config.js
@@ -1,24 +1,25 @@
-const { Client } = require('pg');
+// const { Client } = require('pg');
 
 exports.getTime = function() {
-	var now;console.log("Call get");
+	var now="OKKKKKKKK";
+	console.log("Call get");
 
-	const client = new Client({
-	  connectionString: process.env.DATABASE_URL,
-	  ssl: true
-	});
-	var now;console.log("Creat OBJ");
-	client.connect();
-	var now;console.log("connect OBJ");
+	// const client = new Client({
+	//   connectionString: process.env.DATABASE_URL,
+	//   ssl: true
+	// });
+	// var now;console.log("Creat OBJ");
+	// client.connect();
+	// var now;console.log("connect OBJ");
 
-	client.query('SELECT NOW()', function(err, res) {
-		var now;console.log("query");
-		if (err) {console.log("Err");return;}
-	  for (let row of res.rows) {
-	    console.log(JSON.stringify(row));
-	  }
-		now = {rk:"OK"};
-	});
+	// client.query('SELECT NOW()', function(err, res) {
+	// 	var now;console.log("query");
+	// 	if (err) {console.log("Err");return;}
+	//   for (let row of res.rows) {
+	//     console.log(JSON.stringify(row));
+	//   }
+	// 	now = {rk:"OK"};
+	// });
 	return now;
 }
 // console.log(body);
