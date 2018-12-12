ed377c727365187a3ceb3ee7fa60dcf0c5557da9
Tin Cya <32213532+nghichcode@users.noreply.github.com>
2018-12-12 04:55:49 +0000

    vms1
    sa

--- config.js
+++ config.js
@@ -1,25 +1,23 @@
-// const { Client } = require('pg');
+const { Client } = require('pg');
 
 exports.getTime = function() {
-	var now="OKKKKKKKK";
-	console.log("Call get");
+	var now="Errrr";
 
-	// const client = new Client({
-	//   connectionString: process.env.DATABASE_URL,
-	//   ssl: true
-	// });
-	// var now;console.log("Creat OBJ");
-	// client.connect();
-	// var now;console.log("connect OBJ");
+	const client = new Client({
+	  connectionString: process.env.DATABASE_URL,
+	  ssl: true
+	});
+	client.connect();
 
 	// client.query('SELECT NOW()', function(err, res) {
-	// 	var now;console.log("query");
+	// 	console.log("query");
 	// 	if (err) {console.log("Err");return;}
 	//   for (let row of res.rows) {
 	//     console.log(JSON.stringify(row));
 	//   }
 	// 	now = {rk:"OK"};
 	// });
+
 	return now;
 }
 // console.log(body);
