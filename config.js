056baa888a72fc45e620a2642c0b57d58302ba32
Tin Cya <32213532+nghichcode@users.noreply.github.com>
2018-12-12 07:28:40 +0000

    v118
    s

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ const {Client} = require('pg');
 
 exports.getTime = function() {
 	var now="Errrr";
+	var self = this;
+	this.b=false;
 
 	now = {status: "Error",statusText:"getTime 2"};
 	const client = new Client({
@@ -11,16 +13,20 @@ exports.getTime = function() {
 	client.connect();
 	now = {status: "Error",statusText:"getTime 3"};
 
-	client.query('SELECT NOW();', function(err, res) {
+	client.query('SELECT NOW();', (err, res) => {
+		now = {status: "Error",statusText:"getTime 5"};
 		if (err) {
-			now ={status: "Error",statusText:"getTime Err"} ;
+			self.b={status: "Error",statusText:"getTime Err"};
+			now ={status: "Error",statusText:"getTime Err"};
 		} else {
+			self.b={status: "Error",statusText:"getTime OK"};
 			now = {status: "Error",statusText:"getTime OK"};
 		}
 		client.end();
 	});
 	now = {status: "Error",statusText:"getTime 4"};
 
+	if (self.b!= false) {return self.b;}
 	return now;
 }
 // console.log(body);
