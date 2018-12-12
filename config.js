f2b70f1c164fdcc74fa0ecbeb9544150ab1fb2f0
Tin Cya <32213532+nghichcode@users.noreply.github.com>
2018-12-12 04:12:47 +0000

    vk
    ck

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const {Client} = require('pg');
 var connect = "postgres://khxbijdhccsjkc:80fbc29fa8a5273e15db91d3b704ca76e7d7c8a024bb128b0d8342afaeb18a7f@ec2-54-204-40-248.compute-1.amazonaws.com:5432/d77n0qu161p9b3";
 
 exports.getTime = function() {
-	var now = "Err";
+	var now;
 
 	const client = new Client({
 	  connectionString: connect
@@ -10,7 +10,8 @@ exports.getTime = function() {
 	client.connect();
 
 	client.query('SELECT NOW()', function(err, res) {
-		return {err, res};
+		now = {err, res};
 	});
+	return now;
 }
 // console.log(body);
