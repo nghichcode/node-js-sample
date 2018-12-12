46ddc0a3c97513d722b342977d5e4f95c28a92ee
Tin Cya <32213532+nghichcode@users.noreply.github.com>
2018-12-12 04:17:29 +0000

    v3
    v3

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-const {Client} = require('pg');
-var connect = "postgres://khxbijdhccsjkc:80fbc29fa8a5273e15db91d3b704ca76e7d7c8a024bb128b0d8342afaeb18a7f@ec2-54-204-40-248.compute-1.amazonaws.com:5432/d77n0qu161p9b3";
+const { Client } = require('pg');
 
 exports.getTime = function() {
 	var now;
 
 	const client = new Client({
-	  connectionString: connect
+	  connectionString: process.env.DATABASE_URL,
+	  ssl: true
 	});
 	client.connect();
 
