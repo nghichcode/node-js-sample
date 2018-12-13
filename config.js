08beae9027371e9638e24bc7ff267bf48b28f7d8
Tin Cya <32213532+nghichcode@users.noreply.github.com>
2018-12-13 03:28:05 +0000

    v119
    cca

--- config.js
+++ config.js
@@ -1,32 +1,43 @@
-const {Client} = require('pg');
+const pg = require('pg');
 
-exports.getTime = function() {
-	var now="Errrr";
-	var self = this;
-	this.b=false;
+exports.getTime = function(clientRes) {
+	const conf = {
+		local: {
+		  user:"postgres",
+		  database:"nodeex",
+		  port:5433,
+		  host:"localhost",
+		  password:"12345678"
+		},
+		server: {
+		  user:"khxbijdhccsjkc",
+		  database:"d77n0qu161p9b3",
+		  port:5432,
+		  host:"ec2-54-204-40-248.compute-1.amazonaws.com",
+		  password:"80fbc29fa8a5273e15db91d3b704ca76e7d7c8a024bb128b0d8342afaeb18a7f",
+		  ssl:true
+	}};
 
-	now = {status: "Error",statusText:"getTime 2"};
-	const client = new Client({
-	  connectionString: process.env.DATABASE_URL,
-	  ssl: true
+	const client = new pg.Client(conf.server);
+	client.connect(function (err) {
+		clientRes.writeHead(200,{'Content-Type':'text/json'});
+		clientRes.write(JSON.stringify({status: "C Error", statusText: err}));
+		clientRes.end();
+		console.log(client);
+		client.end();
 	});
-	client.connect();
-	now = {status: "Error",statusText:"getTime 3"};
 
-	client.query('SELECT NOW();', (err, res) => {
-		now = {status: "Error",statusText:"getTime 5"};
+	client.query('SELECT NOW();', function (err, res) {
 		if (err) {
-			self.b={status: "Error",statusText:"getTime Err"};
-			now ={status: "Error",statusText:"getTime Err"};
+			clientRes.writeHead(200,{'Content-Type':'text/json'});
+			clientRes.write(JSON.stringify({status: "Q Error",statusText: err}));
+			clientRes.end();
 		} else {
-			self.b={status: "Error",statusText:"getTime OK"};
-			now = {status: "Error",statusText:"getTime OK"};
+			clientRes.writeHead(200,{'Content-Type':'text/json'});
+			clientRes.write(JSON.stringify({o:1}));
+			clientRes.end();
 		}
 		client.end();
 	});
-	now = {status: "Error",statusText:"getTime 4"};
-
-	if (self.b!= false) {return self.b;}
-	return now;
 }
 // console.log(body);
